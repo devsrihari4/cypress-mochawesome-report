cd2d57694013d55d5c7e8af5f0a4ed6972dc3eb5
sri <sri@sris-MBP.attlocal.net>
2025-04-17 23:46:57 -0500

    Video on failure only

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  video: true,
   reporterOptions: {
     charts: true,
     reportPageTitle: 'custom-title',
     embeddedScreenshots: true,
     inlineAssets: true,
+    videoOnFailOnly: false,
+    saveAllAttempts: false
   },
   e2e: {
     setupNodeEvents(on, config) {
