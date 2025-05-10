4c39b3b7fbb928759c2558cdfafad8cdf7e2984c
sri <sri@sris-MBP.attlocal.net>
2025-05-09 20:29:06 -0500

    Add new report

--- test.config.js
+++ test.config.js
@@ -1,20 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  video: true,
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    videoOnFailOnly: false,
-    saveAllAttempts: false
-  },
+  // reporter: 'cypress-mochawesome-reporter',
+  reporter: 'mochawesome',
+  // video: true,
+  // reporterOptions: {
+  //   charts: true,
+  //   reportPageTitle: 'custom-title',
+  //   embeddedScreenshots: true,
+  //   inlineAssets: true,
+  //   videoOnFailOnly: false,
+  //   saveAllAttempts: false
+  // },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
